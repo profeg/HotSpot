2f8a00f585215d2b4aba0858fd1ecc78ae43a60a
Sergei Koretskiy <sergei.koretskiy@gmail.com>
2016-08-25 14:05:41 +0300

    add ajax request for get json hotspots

--- lib/app.rb
+++ lib/app.rb
@@ -15,7 +15,13 @@ require './lib/hotspot_collection'
 class SinatraApp < Sinatra::Base
   # use Rack::SSL
   register Sinatra::Shopify
-  
+
+  before '/gallery_hotspot_collection_json/:collection_id' do
+    content_type :json
+    headers 'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Methods' => ['OPTIONS', 'GET', 'POST']
+  end
+
   set :scope, 'read_products, read_orders'
   # Change DB in production
   set :database, 'mysql2://debian-sys-maint:TThoEfMEJBMhgQnG@localhost/hotspot_dev'
@@ -27,9 +33,9 @@ class SinatraApp < Sinatra::Base
 
   get '/products.json' do
     shopify_session do
-      @products = ShopifyAPI::Product.find(:all, params: { limit: 250 }) 
+      @products = ShopifyAPI::Product.find(:all, params: {limit: 250})
       content_type :json
-        @products.to_json
+      @products.to_json
     end
   end
 
@@ -37,27 +43,27 @@ class SinatraApp < Sinatra::Base
     shopify_session do
       @collections = ShopifyAPI::CustomCollection.all
       content_type :json
-        @collections.to_json
+      @collections.to_json
     end
   end
 
   get '/hotspot_collection.json/:collection_id' do
     shopify_session do
-      @hotspot_collections = HotspotCollection.find_by( collection_id: params[:collection_id] )
+      @hotspot_collections = HotspotCollection.find_by(collection_id: params[:collection_id])
       content_type :json
-        @hotspot_collections.to_json
+      @hotspot_collections.to_json
     end
   end
 
   get '/interface.json/:id' do
     shopify_session do
-      Interface.find( params['id'] ).to_json
+      Interface.find(params['id']).to_json
     end
   end
 
   get '/interfaces.json/:hotspot_collection_id' do
     shopify_session do
-      @interfaces = Interface.where( hotspot_collection_id: params[:hotspot_collection_id] )
+      @interfaces = Interface.where(hotspot_collection_id: params[:hotspot_collection_id])
       @interfaces.to_json
     end
   end
@@ -72,25 +78,25 @@ class SinatraApp < Sinatra::Base
   post '/save_hotspot_collection' do
     shopify_session do
       params = JSON.parse request.body.read
-      if @hotspot_collection = HotspotCollection.find_by( collection_id: params['collection_id'] )
+      if @hotspot_collection = HotspotCollection.find_by(collection_id: params['collection_id'])
         @hotspot_collection.update_attributes params
       else
-        HotspotCollection.create( title: params['title'], collection_id: params['collection_id'] ) # params here
-      end  
+        HotspotCollection.create(title: params['title'], collection_id: params['collection_id']) # params here
+      end
     end
   end
 
   post '/save_interface' do
     shopify_session do
       params = JSON.parse request.body.read
-      if @interface = Interface.find_by( hotspot_collection_id: params['collection_id'], title: params['title'], image: params['image'] )
+      if @interface = Interface.find_by(hotspot_collection_id: params['collection_id'], title: params['title'], image: params['image'])
         @interface.update_attributes({
-          hotspot_collection_id: params['collection_id'], 
-          title: params['title']
-        })
+                                         hotspot_collection_id: params['collection_id'],
+                                         title: params['title']
+                                     })
       else
-        @interface = Interface.create( hotspot_collection_id: params['collection_id'], title: params['title'], image: params['image'] )
-      end   
+        @interface = Interface.create(hotspot_collection_id: params['collection_id'], title: params['title'], image: params['image'])
+      end
       @hotspot_collection = HotspotCollection.find params['collection_id']
       @hotspot_collection.interfaces << @interface
       @hotspot_collection.save
@@ -102,7 +108,7 @@ class SinatraApp < Sinatra::Base
       params = JSON.parse request.body.read
       interface = Interface.find params['interface_id']
       interface.hotspots = params['hotspots'].collect do |hotspot|
-        Hotspot.create x: hotspot['x'], 
+        Hotspot.create x: hotspot['x'],
                        y: hotspot['y'],
                        icon_scale: hotspot['icon_scale']
       end
@@ -110,6 +116,21 @@ class SinatraApp < Sinatra::Base
     end
   end
 
+  get '/gallery_hotspot_collection_json/:collection_id' do
+    @json_hotspots = {}
+    @collection_id = params[:collection_id]
+    @hotspot_collection = HotspotCollection.find_by(collection_id: @collection_id)
+    unless @hotspot_collection.nil?
+      @interface = Interface.find_by(hotspot_collection_id: @hotspot_collection.id)
+      @hotspots = Hotspot.where(interface_id: @interface.id)
+      @hotspots.each_with_index do |hotspot, index|
+        @json_hotspots[index] = {'collection_id' => @collection_id, 'x' => hotspot.x, 'y' => hotspot.y,
+                                 'icon_scale' => hotspot.icon_scale}
+      end
+    end
+    content_type :json
+    @json_hotspots.to_json
+  end
 
 
   post '/uninstall' do
@@ -132,7 +153,6 @@ class SinatraApp < Sinatra::Base
   # }
   # Rack::Server.start webrick_options
 
-  
 
   private
 
